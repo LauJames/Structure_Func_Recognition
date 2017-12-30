9aa10fad9fcfeb6020b2aae33f4e7614f4853963
LauJames <LauJames_work@163.com>
2017-12-30 23:57:13 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Structure_Func_Recognition
---
+++ dataHelper.py
@@ -0,0 +1,94 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : StructureFuncRecognition 
+@File     : dataHelper.py
+@Time     : 2017/12/26 10:38
+@Software : PyCharm
+@Copyright: "Copyright (c) 2017 Lau James. All Rights Reserved"
+"""
+
+import numpy as np
+import re
+import codecs
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    :param string:
+    :return: string
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    :param data:
+    :param batch_size:
+    :param num_epochs:
+    :param shuffle:
+    :return:
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batch_per_epoch = int((len(data) - 1) / batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffle_data = data[shuffle_indices]
+        else:
+            shuffle_data = data
+        for batch_num in range(num_batch_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffle_data[start_index: end_index]
+
+
+def get_para_label(file_path):
+    """
+    load data from files, splits the data into words and labels
+    :return: paras, labels
+    """
+    paras = []
+    labels = []
+    with codecs.open(file_path, encoding='utf-8') as fp:
+        while True:
+            line = fp.readline()
+            if not line:
+                print("Data loaded successfully!")
+                paras = [clean_str(paragraph) for paragraph in paras]
+                return [paras, np.array(labels)]
+            tmp = line.strip().split('\t')[-2:]
+            label, para = int(tmp[0]), tmp[1]
+            if label == 1:
+                labels.append([1, 0, 0, 0, 0])
+            elif label == 2:
+                labels.append([0, 1, 0, 0, 0])
+            elif label == 3:
+                labels.append([0, 0, 1, 0, 0])
+            elif label == 4:
+                labels.append([0, 0, 0, 1, 0])
+            else:
+                labels.append([0, 0, 0, 0, 1])
+            paras.append(para)
