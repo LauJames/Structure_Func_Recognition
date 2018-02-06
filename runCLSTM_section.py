8a3ed285a534df8533fccf2a1d322c1205b15dc1
LauJames <LauJames_work@163.com>
2018-02-06 15:03:03 +0800

    + add CNN/RNN/CLSTM runner for section level

---
+++ runCLSTM_section.py
@@ -0,0 +1,13 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : Structure_Func_Recognition 
+@File     : runCLSTM_section.py
+@Time     : 2018/2/6 15:01
+@Software : PyCharm
+@Copyright: "Copyright (c) 2017 Lau James. All Rights Reserved"
+"""
---
+++ runCNN_section.py
@@ -0,0 +1,14 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : Structure_Func_Recognition 
+@File     : runCNN_section.py
+@Time     : 2018/2/6 15:00
+@Software : PyCharm
+@Copyright: "Copyright (c) 2017 Lau James. All Rights Reserved"
+"""
+
