0c53c71dd7f2cac4846eb289ff26ed7bcc075ef4
LauJames <LauJames_work@163.com>
2018-03-02 13:18:00 +0800

    * sequence length tuning to 2500

--- runCNN_section.py
+++ runCNN_section.py
@@ -37,7 +37,7 @@ tf.flags.DEFINE_string("save_dir", "checkpoints/textCNN_section", "save base dir
 tf.flags.DEFINE_integer("embedding_dim", 256, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_integer("vocab_size", 119396, "vocabulary size (default: 5000)")
 tf.flags.DEFINE_integer("num_classes", 5, "Number of classes (default: 5)")
-tf.flags.DEFINE_integer("seq_length", 5000, "sequence length (default: 600)")
+tf.flags.DEFINE_integer("seq_length", 2500, "sequence length (default: 5000)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
