bfbc0af55801af7eb1508de863f4263d4a6e7626
LauJames <LauJames_work@163.com>
2019-04-11 11:42:14 +0800

    + add self attention model run file

---
+++ data/runTF_header.py
@@ -0,0 +1,14 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : Structure_Func_Recognition 
+@File     : runTF_header.py
+@Time     : 2019/4/11 11:39
+@Software : PyCharm
+@Copyright: "Copyright (c) 2018 Lau James. All Rights Reserved"
+"""
+
